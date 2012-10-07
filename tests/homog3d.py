033322f0ec68fa351b3b71e32e3e7830e0225390
huskeypm <huskeypm@gmail.com>
2012-10-07 15:48:52 -0700

    gnerelized homo3d a bit

--- tests/homog3d.py
+++ tests/homog3d.py
@@ -3,6 +3,9 @@ import numpy as np
 
 boundsMin = np.zeros(3)
 boundsMax = np.zeros(3)
+case="none"
+meshFile="none"
+dim=-1
 
 EPS = 0.001 
 def DefineBoundary(x,btype,on_boundary):
@@ -11,7 +14,10 @@ def DefineBoundary(x,btype,on_boundary):
 
   lr = ( np.abs(x[0]-boundsMin[0]) < EPS or np.abs(x[0]-boundsMax[0]) < EPS)
   tb = ( np.abs(x[1]-boundsMin[1]) < EPS or np.abs(x[1]-boundsMax[1]) < EPS)
-  fb = ( np.abs(x[2]-boundsMin[2]) < EPS or np.abs(x[2]-boundsMax[2]) < EPS)
+  if(dim==3):
+    fb = ( np.abs(x[2]-boundsMin[2]) < EPS or np.abs(x[2]-boundsMax[2]) < EPS)
+  else:
+    fb=0
   obs = (not tb and not lr and not fb)
 
   if(btype=="lr"):
@@ -50,117 +56,201 @@ class ObsBoundary(SubDomain):
 
 
 
-mine=0
-auriault=1
+def doit(meshFile="none", case="none"):
 # my meshes
-if(mine==1):
-  # works 
-  gamer=1
-  mesh = Mesh("3d.xml.gz") 
-  #
-  #??not sure what this was mesh = Mesh("/home/huskeypm/scratch/120816/test_mesh.xml.gz")
-  for i in np.arange(3):
-    boundsMin[i] = np.min(mesh.coordinates()[:,i])
-    boundsMax[i] = np.max(mesh.coordinates()[:,i])
-
-# aurialt test
-elif(auriault==1):
-  gamer=0
-  meshFile = "half_auriault_3d.xml"
-  mesh = Mesh(meshFile)
-  boundsMin[:]=np.array([0,999,0])
-  boundsMax[:]=np.array([5,999,5])
-
-
-print boundsMin
-print boundsMax
-
-# Test boundaries
-test= 0
-if (test==1):
-  V = FunctionSpace(mesh,"CG",1)
-  x = Function(V)
-  #bc_lr = DirichletBC(V,Constant(1),XBoundary())
-  #bc_lr.apply(x.vector())
-  #bc_obs = DirichletBC(V,Constant(20),ObsBoundary())
-  #bc_obs.apply(x.vector())
-  #bc_tb = DirichletBC(V,Constant(5),YBoundary())
-  #bc_tb.apply(x.vector())
-  bc_fb = DirichletBC(V,Constant(7),ZBoundary())
-  bc_fb.apply(x.vector())
-  File("testbound.pvd") << x
-  quit()
+#case = "cheng"
+#case = "auriault"
+  if(case=="cheng"):
+    # works 
+    gamer=1
+    mesh = Mesh("2d.xml.gz") 
+    mesh = Mesh("3d.xml.gz") 
+    #
+    #??not sure what this was mesh = Mesh("/home/huskeypm/scratch/120816/test_mesh.xml.gz")
+    for i in np.arange( mesh.ufl_cell().geometric_dimension() ):
+      boundsMin[i] = np.min(mesh.coordinates()[:,i])
+      boundsMax[i] = np.max(mesh.coordinates()[:,i])
   
-
-
-V = VectorFunctionSpace(mesh,"CG",1)
-
-bcs = [] 
-if(mine==1):
-  ## e = [1,0,0]
-  bcs.append(DirichletBC(V.sub(0),Constant(0),XBoundary()))
-  # Neumann i believe is nalba w + delta = 0 on boundary, so natural
-  ## e = [0,1,0]
-  bcs.append(DirichletBC(V.sub(1),Constant(0),YBoundary()))
-  # Neumann i believe is nalba w + delta = 0 on boundary, so natural
-  ## e = [0,0,1]
-  bcs.append(DirichletBC(V.sub(2),Constant(0),ZBoundary()))
-  # Neumann i believe is nalba w + delta = 0 on boundary, so natural
-
-elif(auriault==1):
-  ## e = [1,0,0]
-  bcs.append(DirichletBC(V.sub(0),Constant(0),XBoundary()))
-  # Neumann i believe is nalba w + delta = 0 on boundary, so natural
- 
-  # none on Y direction  
- 
-  ## e = [0,0,1]
-  bcs.append(DirichletBC(V.sub(2),Constant(0),ZBoundary()))
-  # Neumann i believe is nalba w + delta = 0 on boundary, so natural
+  # aurialt test
+  elif(case=="auriault"):
+    gamer=0
+    meshFile = "half_auriault.xml" # from Auriault paper
+    meshFile = "half_auriault_3d.xml"
+    mesh = Mesh(meshFile)
+    boundsMin[:]=np.array([0,999,0])
+    boundsMax[:]=np.array([5,999,5])
   
-Dii  = Constant((1.0,1.0,1.0))
-Aij = diag(Dii) 
-Delta = Identity( mesh.ufl_cell().geometric_dimension()) #
-
-u = TrialFunction(V)
-v = TestFunction(V)
-if(gamer==1):
-  form = inner(Aij*(grad(u) + Delta), grad(v))*dx(1)
-else:
-  form = inner(Aij*(grad(u) + Delta), grad(v))*dx
-a = lhs(form)
-L = rhs(form)
-
-x = Function(V)
-solve(a == L, x, bcs)
-#plot(u,interactive=True)
-File("3d.pvd") << x 
-
-dim = mesh.ufl_cell().geometric_dimension()
-omegas = np.zeros(dim)
-for i in range(dim):
-  v = [0,0,0]
-  v[i] = 1
-  grad_Xi_component = inner(grad(x[i]),Constant((v[0],v[1],v[2]))) + Constant(1)
-  outname = "diff%d.pvd" % i
-  Vscalar = FunctionSpace(mesh,"CG",1)
-  File(outname)<<project(grad_Xi_component,V=Vscalar)
-
-  if(gamer==1):
-    form = grad_Xi_component * dx(1)
+  # shorten geom
+  elif(case=="shorten"):
+    print "SHORTEN geometry"
+    meshFile = "shorten.xml" # from Auriault paper
+    #bounds[0,:] = np.array([0,6])
+    #bounds[1,:] = np.array([0,3])    
+    mesh = Mesh(meshFile)     
+    gamer=0
+    #
+    for i in np.arange( mesh.ufl_cell().geometric_dimension() ):
+      boundsMin[i] = np.min(mesh.coordinates()[:,i])
+      boundsMax[i] = np.max(mesh.coordinates()[:,i])
+  
   else:
-    form = grad_Xi_component * dx
-  integrand = assemble(form)
-  omegas[i] = integrand
+    mesh = Mesh(meshFile)     
+    gamer=0
+    #
+    for i in np.arange( mesh.ufl_cell().geometric_dimension() ):
+      boundsMin[i] = np.min(mesh.coordinates()[:,i])
+      boundsMax[i] = np.max(mesh.coordinates()[:,i])
+  
+  
+  dim = mesh.ufl_cell().geometric_dimension()
+  print "Using %d dim mesh" % dim
+  print boundsMin
+  print boundsMax
+  
+  # Test boundaries
+  test= 0
+  if (test==1):
+    V = FunctionSpace(mesh,"CG",1)
+    x = Function(V)
+    #bc_lr = DirichletBC(V,Constant(1),XBoundary())
+    #bc_lr.apply(x.vector())
+    #bc_obs = DirichletBC(V,Constant(20),ObsBoundary())
+    #bc_obs.apply(x.vector())
+    #bc_tb = DirichletBC(V,Constant(5),YBoundary())
+    #bc_tb.apply(x.vector())
+    bc_fb = DirichletBC(V,Constant(7),ZBoundary())
+    bc_fb.apply(x.vector())
+    File("testbound.pvd") << x
+    quit()
+    
+  
+  
+  V = VectorFunctionSpace(mesh,"CG",1)
+  
+  bcs = [] 
+  if(case=="cheng"):
+    ## e = [1,0,0]
+    bcs.append(DirichletBC(V.sub(0),Constant(0),XBoundary()))
+    # Neumann i believe is nalba w + delta = 0 on boundary, so natural
+    ## e = [0,1,0]
+    bcs.append(DirichletBC(V.sub(1),Constant(0),YBoundary()))
+    # Neumann i believe is nalba w + delta = 0 on boundary, so natural
+    ## e = [0,0,1]
+    if(dim==3):
+      bcs.append(DirichletBC(V.sub(2),Constant(0),ZBoundary()))
+      # Neumann i believe is nalba w + delta = 0 on boundary, so natural
+  
+  elif(case=="auriault"):
+    ## e = [1,0,0]
+    bcs.append(DirichletBC(V.sub(0),Constant(0),XBoundary()))
+    # Neumann i believe is nalba w + delta = 0 on boundary, so natural
+   
+    # none on Y direction  
+   
+    ## e = [0,0,1]
+    if(dim==3):
+      bcs.append(DirichletBC(V.sub(2),Constant(0),ZBoundary()))
+    # Neumann i believe is nalba w + delta = 0 on boundary, so natural
+  if(case=="shorten"):
+    bcs.append(DirichletBC(V.sub(0),Constant(0),XBoundary()))
+    bcs.append(DirichletBC(V.sub(1),Constant(0),YBoundary()))
+    if(dim==3):
+      bcs.append(DirichletBC(V.sub(2),Constant(0),ZBoundary()))
+  else:
+    bcs.append(DirichletBC(V.sub(0),Constant(0),XBoundary()))
+    bcs.append(DirichletBC(V.sub(1),Constant(0),YBoundary()))
+    if(dim==3):
+      bcs.append(DirichletBC(V.sub(2),Constant(0),ZBoundary()))
+    
+  if(dim==2):
+    Dii  = Constant((1.0,1.0))
+  elif(dim==3):
+    Dii  = Constant((1.0,1.0,1.0))
+  Aij = diag(Dii) 
+  #Delta = Identity( mesh.ufl_cell().geometric_dimension()) #
+  Delta = Identity( dim )
+  
+  u = TrialFunction(V)
+  v = TestFunction(V)
+  if(gamer==1):
+    form = inner(Aij*(grad(u) + Delta), grad(v))*dx(1)
+  else:
+    form = inner(Aij*(grad(u) + Delta), grad(v))*dx
+  a = lhs(form)
+  L = rhs(form)
+  
+  x = Function(V)
+  solve(a == L, x, bcs)
+  #plot(u,interactive=True)
+  name = "%dd.pvd" % dim
+  File(name) << x 
+  
+  omegas = np.zeros(dim)
+  for i in range(dim):
+    v = [0,0,0]
+    v[i] = 1
+    if(dim==2):
+      grad_Xi_component = inner(grad(x[i]),Constant((v[0],v[1]))) + Constant(1)
+    elif(dim==3):
+      grad_Xi_component = inner(grad(x[i]),Constant((v[0],v[1],v[2]))) + Constant(1)
+    outname = "diff%d.pvd" % i
+    Vscalar = FunctionSpace(mesh,"CG",1)
+    File(outname)<<project(grad_Xi_component,V=Vscalar)
+  
+    if(gamer==1):
+      form = grad_Xi_component * dx(1)
+    else:
+      form = grad_Xi_component * dx
+    integrand = assemble(form)
+    omegas[i] = integrand
+  
+  if(gamer==1):
+    vol = assemble( Constant(1)*dx(1),mesh=mesh ) 
+    surf = assemble( Constant(1)*ds,mesh=mesh ) 
+  else:
+    vol = assemble( Constant(1)*dx,mesh=mesh ) 
+    surf = assemble( Constant(1)*ds,mesh=mesh ) 
+  
+  print "D*"
+  Ds = omegas/vol
+  print Ds
+  print "Dx"
+  print Ds[0]
+  
+  # for debug
+  # Only relevant for cubes
+  #print "Vol bounds  volfrac  2/3p 2p/2-p"
+  #v = np.prod( (boundsMax-boundsMin)[0:dim])
+  #vf = vol /v
+  #print "%f %f %f %f" % (v,vf,2/3.*vf, 2*vf/(3-vf))
 
-if(gamer==1):
-  vol = assemble( Constant(1)*dx(1),mesh=mesh ) 
-  surf = assemble( Constant(1)*ds,mesh=mesh ) 
-else:
-  vol = assemble( Constant(1)*dx,mesh=mesh ) 
-  surf = assemble( Constant(1)*ds,mesh=mesh ) 
+if __name__ == "__main__":
+  msg="""
+Purpose: 
 
-print "D*"
-print omegas/vol
+Usage:
+  homog3d.py <-case or -mesh>
+
+Notes:
+"""
+
+  import sys
+  if len(sys.argv) < 2:
+      raise RuntimeError(msg)
+
+  fileIn= sys.argv[1]
+  if(len(sys.argv)==3):
+    print "arg"
+
+  for i,arg in enumerate(sys.argv):
+    if(arg=="-case"):
+      case=sys.argv[i+1]
+    if(arg=="-mesh"):
+      meshFile=sys.argv[i+1]
 
 
+
+
+
+
+  doit(case=case,meshFile=meshFile) 
